a44af246d15feffe523667b4ce13d75936bcbf36
dr-frmr <docterformer@protonmail.com>
2024-11-27 21:20:14 -0500

    use app framework

--- canvas-demo/src/lib.rs
+++ canvas-demo/src/lib.rs
@@ -1,8 +1,8 @@
 use crate::kinode::process::canvas_demo::{
     Point, Request as CanvasRequest, Response as CanvasResponse,
 };
-use kinode_process_lib::{
-    await_message, call_init, http, println, Address, LazyLoadBlob, Message, Request, Response,
+use kinode_app_framework::{
+    app, http, kiprintln, send_ws_update, Address, Message, Request, Response,
 };
 use std::collections::{HashMap, HashSet};
 
@@ -10,10 +10,22 @@ wit_bindgen::generate!({
     path: "target/wit",
     world: "canvas-demo-template-dot-os-v0",
     generate_unused_types: true,
-    additional_derives: [serde::Serialize, serde::Deserialize, process_macros::SerdeJsonInto],
+    additional_derives: [serde::Serialize, serde::Deserialize, kinode_app_framework::SerdeJsonInto],
 });
 
-type State = HashMap<String, Canvas>;
+app!(
+    "Canvas Demo",
+    None,
+    None,
+    handle_api_call,
+    handle_remote_request
+);
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct State {
+    our: Address,
+    canvases: HashMap<String, Canvas>,
+}
 
 #[derive(Default, serde::Serialize, serde::Deserialize)]
 struct Canvas {
@@ -21,8 +33,16 @@ struct Canvas {
     points: Vec<Point>,
 }
 
+impl kinode_app_framework::State for State {
+    fn new(our: Address) -> Self {
+        let mut canvases = HashMap::new();
+        canvases.insert(our.node().to_string(), Canvas::default());
+        Self { our, canvases }
+    }
+}
+
 #[derive(serde::Serialize, serde::Deserialize)]
-enum ApiCall {
+enum Api {
     AddUser(String),
     RemoveUser(String),
     Draw((String, Point)),
@@ -30,166 +50,90 @@ enum ApiCall {
     GetCanvas(String),
 }
 
-call_init!(init);
-fn init(our: Address) {
-    // serve frontend UI to user
-    let mut server = http::server::HttpServer::new(5);
-
-    server
-        .serve_ui(
-            &our,
-            "ui",
-            vec!["/"],
-            http::server::HttpBindingConfig::default(),
-        )
-        .expect("failed to serve UI");
-
-    server
-        .bind_http_path("/api", http::server::HttpBindingConfig::default())
-        .expect("failed to serve API path");
-
-    server
-        .bind_ws_path("/updates", http::server::WsBindingConfig::default())
-        .expect("failed to bind WS path");
-
-    let mut state = State::default();
-
-    // create a canvas for ourselves
-    let mut our_canvas = Canvas::default();
-    our_canvas.users.insert(our.node().to_string());
-    state.insert(our.node().to_string(), our_canvas);
-
-    loop {
-        match await_message() {
-            Err(send_error) => println!("got SendError: {send_error}"),
-            Ok(ref message) => handle_message(&our, &mut state, message, &mut server),
-        }
-    }
-}
-
-fn handle_message(
-    our: &Address,
+fn handle_api_call(
+    _message: &Message,
     state: &mut State,
-    message: &Message,
-    server: &mut http::server::HttpServer,
-) {
-    if message.is_local(our) {
-        // handle local messages
-        if message.source().process == "http_server:distro:sys" {
-            handle_http_request(our, message, state, server);
-        }
-    } else {
-        // handle remote messages
-        handle_remote_request(our, message, state, server);
-    }
-}
+    call: Api,
+) -> (http::server::HttpResponse, Vec<u8>) {
+    let ok_response = http::server::HttpResponse::new(200 as u16);
 
-fn handle_http_request(
-    our: &Address,
-    message: &Message,
-    state: &mut State,
-    server: &mut http::server::HttpServer,
-) {
-    let http_request = serde_json::from_slice::<http::server::HttpServerRequest>(&message.body())
-        .expect("failed to parse HTTP request");
-
-    server.handle_request(
-        http_request,
-        |_incoming| {
-            let response = http::server::HttpResponse::new(200 as u16);
-            let mut blob: Option<LazyLoadBlob> = None;
-
-            let Some(Ok(call)) = message
-                .blob()
-                .map(|blob| serde_json::from_slice::<ApiCall>(blob.bytes()))
+    match call {
+        Api::AddUser(user) => {
+            // *we* want to add user to our canvas
+            let canvas = state.canvases.get_mut(state.our.node()).unwrap();
+            canvas.users.insert(user.clone());
+            let Ok(invite_response) = Request::to((&user, state.our.process.clone()))
+                .body(CanvasRequest::AddUser(user))
+                .blob_bytes(serde_json::to_vec(&canvas).unwrap())
+                .send_and_await_response(5)
+                .unwrap()
             else {
-                return (response.set_status(400), blob);
+                return (ok_response.set_status(500), vec![]);
             };
-
-            match call {
-                ApiCall::AddUser(user) => {
-                    // *we* want to add user to our canvas
-                    let canvas = state.get_mut(our.node()).unwrap();
-                    canvas.users.insert(user.clone());
-                    let Ok(invite_response) = Request::to((&user, our.process.clone()))
-                        .body(CanvasRequest::AddUser(user))
-                        .blob_bytes(serde_json::to_vec(&canvas).unwrap())
-                        .send_and_await_response(5)
-                        .unwrap()
-                    else {
-                        return (response.set_status(500), blob);
-                    };
-                    match invite_response.body().try_into() {
-                        Ok(CanvasResponse::InviteAccepted) => (),
-                        _ => return (response.set_status(502), blob),
-                    }
-                }
-                ApiCall::RemoveUser(user) => {
-                    // *we* want to remove user from our canvas
-                    let users = &state.get(our.node()).unwrap().users;
-
-                    for target in users {
-                        Request::to((target, our.process.clone()))
-                            .body(CanvasRequest::RemoveUser(user.clone()))
-                            .send()
-                            .unwrap();
-                    }
-
-                    state.get_mut(our.node()).unwrap().users.remove(&user);
-                }
-                ApiCall::Draw((canvas_id, point)) => {
-                    // *we* want to draw on a canvas
-                    // if it's our canvas, broadcast the draw to all users
-                    // otherwise, just send to the owner
-                    if canvas_id == our.node() {
-                        let canvas = state.get_mut(&canvas_id).unwrap();
-                        canvas.points.push(point.clone());
-                        for target in canvas.users.iter() {
-                            Request::to((target, our.process.clone()))
-                                .body(CanvasRequest::Draw((canvas_id.clone(), point.clone())))
-                                .send()
-                                .unwrap();
-                        }
-                    } else {
-                        Request::to((&canvas_id, our.process.clone()))
-                            .body(CanvasRequest::Draw((canvas_id.clone(), point.clone())))
-                            .send()
-                            .unwrap();
-                    }
-                }
-                ApiCall::GetCanvasList => {
-                    blob = Some(LazyLoadBlob::new(
-                        Some("application/json"),
-                        serde_json::to_vec(&state.keys().collect::<Vec<_>>()).unwrap(),
-                    ));
-                }
-                ApiCall::GetCanvas(canvas_id) => {
-                    blob = Some(LazyLoadBlob::new(
-                        Some("application/json"),
-                        serde_json::to_vec(&state.get(&canvas_id)).unwrap(),
-                    ));
-                }
+            match invite_response.body().try_into() {
+                Ok(CanvasResponse::InviteAccepted) => (ok_response, vec![]),
+                _ => (ok_response.set_status(502), vec![]),
             }
-
-            (response, blob)
-        },
-        |_, _, _| {
-            // skip incoming ws requests
-        },
-    );
+        }
+        Api::RemoveUser(user) => {
+            // *we* want to remove user from our canvas
+            let users = &state.canvases.get(state.our.node()).unwrap().users;
+            for target in users {
+                Request::to((target, state.our.process.clone()))
+                    .body(CanvasRequest::RemoveUser(user.clone()))
+                    .send()
+                    .unwrap();
+            }
+            state
+                .canvases
+                .get_mut(state.our.node())
+                .unwrap()
+                .users
+                .remove(&user);
+            (ok_response, vec![])
+        }
+        Api::Draw((canvas_id, point)) => {
+            // *we* want to draw on a canvas
+            // if it's our canvas, broadcast the draw to all users
+            // otherwise, just send to the owner
+            if canvas_id == state.our.node() {
+                let canvas = state.canvases.get_mut(&canvas_id).unwrap();
+                canvas.points.push(point.clone());
+                for target in canvas.users.iter() {
+                    Request::to((target, state.our.process.clone()))
+                        .body(CanvasRequest::Draw((canvas_id.clone(), point.clone())))
+                        .send()
+                        .unwrap();
+                }
+            } else {
+                Request::to((&canvas_id, state.our.process.clone()))
+                    .body(CanvasRequest::Draw((canvas_id.clone(), point.clone())))
+                    .send()
+                    .unwrap();
+            }
+            (ok_response, vec![])
+        }
+        Api::GetCanvasList => (
+            ok_response,
+            serde_json::to_vec(&state.canvases.keys().collect::<Vec<_>>()).unwrap(),
+        ),
+        Api::GetCanvas(canvas_id) => (
+            ok_response,
+            serde_json::to_vec(&state.canvases.get(&canvas_id)).unwrap(),
+        ),
+    }
 }
 
 fn handle_remote_request(
-    our: &Address,
     message: &Message,
     state: &mut State,
     server: &mut http::server::HttpServer,
+    request: CanvasRequest,
 ) {
-    match message.body().try_into() {
-        Ok(CanvasRequest::AddUser(user)) => {
+    match request {
+        CanvasRequest::AddUser(user) => {
             let canvas_id = message.source().node();
-            println!("{user} got added to canvas {canvas_id}");
-            if user == our.node() {
+            if user == state.our.node() {
                 // someone wants to add us to their canvas
                 // let's automatically accept for now
                 let Ok(canvas) =
@@ -201,54 +145,45 @@ fn handle_remote_request(
                         .unwrap();
                     return;
                 };
-                state.insert(canvas_id.to_string(), canvas);
+                state.canvases.insert(canvas_id.to_string(), canvas);
                 Response::new()
                     .body(CanvasResponse::InviteAccepted)
                     .send()
                     .unwrap();
-            } else if let Some(canvas) = state.get_mut(canvas_id) {
+            } else if let Some(canvas) = state.canvases.get_mut(canvas_id) {
                 // owner adds someone else to canvas
                 canvas.users.insert(user);
             }
         }
-        Ok(CanvasRequest::RemoveUser(user)) => {
+        CanvasRequest::RemoveUser(user) => {
             let canvas_id = message.source().node();
-            println!("{user} got removed from canvas {canvas_id}");
-            if user == our.node() {
+            if user == state.our.node() {
                 // someone wants to remove us from their canvas
-                state.remove(canvas_id);
-            } else if let Some(canvas) = state.get_mut(canvas_id) {
+                state.canvases.remove(canvas_id);
+            } else if let Some(canvas) = state.canvases.get_mut(canvas_id) {
                 canvas.users.remove(&user);
             }
         }
-        Ok(CanvasRequest::Draw((canvas_id, point))) => {
+        CanvasRequest::Draw((canvas_id, point)) => {
             let user = message.source().node();
-            println!("{user} drew {point:?} on canvas {canvas_id}");
-
-            let Some(canvas) = state.get_mut(&canvas_id) else {
-                println!("user {user} tried to draw on non-existent canvas {canvas_id}");
+            let Some(canvas) = state.canvases.get_mut(&canvas_id) else {
+                kiprintln!("user {user} tried to draw on non-existent canvas {canvas_id}");
                 return;
             };
-
             // validate that user is in canvas
             if !canvas.users.contains(user) {
-                println!("user {user} is not in canvas {canvas_id}");
+                kiprintln!("user {user} is not in canvas {canvas_id}");
                 return;
             }
-
-            server.ws_push_all_channels(
-                "/updates",
-                http::server::WsMessageType::Text,
-                LazyLoadBlob::new(
-                    Some("application/json"),
-                    serde_json::to_vec(&(canvas_id.clone(), point.clone())).unwrap(),
-                ),
+            // draw point on frontend
+            send_ws_update(
+                &server,
+                serde_json::to_vec(&(canvas_id.clone(), point.clone())).unwrap(),
             );
-
-            if canvas_id == our.node() {
+            if canvas_id == state.our.node() {
                 // we need to push to everyone
                 for target in canvas.users.iter() {
-                    Request::to((target, our.process.clone()))
+                    Request::to((target, state.our.process.clone()))
                         .body(CanvasRequest::Draw((canvas_id.clone(), point.clone())))
                         .send()
                         .unwrap();
@@ -258,6 +193,5 @@ fn handle_remote_request(
                 canvas.points.push(point);
             }
         }
-        Err(e) => println!("failed to parse request: {e}"),
     }
 }
